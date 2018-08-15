4fcf83b70317bf80055545dd0c14341144299f1e
Donnie <donnie.d.yuan@outlook.com>
2018-08-15 16:26:18 +0800

    添加项目文件。

---
+++ Elastic/Program.cs
@@ -0,0 +1,35 @@
+﻿using Nest;
+using System;
+
+namespace Elastic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Index、Type ElasticSearch 建议小写
+            ConnectionSettings connectionSetting = new ConnectionSettings(new Uri("http://10.10.21.214:9200")).DefaultIndex("order");
+
+            ElasticClient elasticClient = new ElasticClient(connectionSetting);
+
+            //匹配全部
+            var matchAll = elasticClient.Search<SaleOrder>(s => s
+                .Type("saleOrder")
+                .Query(q => q.MatchAll()));
+
+            //单模糊匹配
+            var singleFuzzy = elasticClient.Search<SaleOrder>(s => s
+                .Type("saleOrder")
+                .Query(q => q.QueryString(t => t.Fields(x => x.Field("SaleOrderDetails.FullName")).Query("纸"))));
+
+            //多模糊匹配
+            var mutilFuzzy = elasticClient.Search<SaleOrder>(s => s
+                .Type("saleOrder")
+                .Query(q => q.Bool(b => b.Must(p =>
+                    p.QueryString(t => t.Fields(x => x.Field("SaleOrderDetails.FullName")).Query("纸")) ||
+                    p.QueryString(t => t.Fields(x => x.Field("SaleOrderDetails.FullName")).Query("银行"))))));
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Elastic/SaleOrder.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace Elastic
+{
+    public class SaleOrder
+    {
+        public int SaleOrderId { get; set; }
+
+        public string SaleOrderCode { get; set; }
+
+        public List<SaleOrderDetail> SaleOrderDetails { get; set; }
+    }
+
+    public class SaleOrderDetail
+    {
+        public int SaleOrderDetailId { get; set; }
+
+        public string FullName { get; set; }
+    }
+}
